f6ec9e8a73359abc3d5ac8c0c3c8d224a43cf818
SignMedici <amenophis170981@gmail.com>
2022-03-24 19:30:02 +0100

    update: route to contact theMovieDB

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(httpHeaders);
 app.use("/api/auth/", authRouter);
 app.use("/api/movies/", movieRouter);
 app.use("/api/users/", userRouter);
-app.use("/api/remote/", theMovideDBRouter);
+app.use("/api/the-movie-db/", theMovideDBRouter);
 
 app.use(serverErrorManager);
 
