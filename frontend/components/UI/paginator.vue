7dce14a669a3a2e843f3196e241599be5a72ea01
SignMedici <amenophis170981@gmail.com>
2022-03-27 23:48:53 +0200

    feat: paginator component almost ready

---
+++ frontend/components/UI/paginator.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="text-center">
+    <v-pagination
+      v-model="pagination.page"
+      @input="next"
+      :length="pagination.total / pagination.perPage"
+      prev-icon="mdi-menu-left"
+      next-icon="mdi-menu-right"
+      color="var(--color-fushia)"
+    ></v-pagination>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      pagination: {
+        page: 1,
+        total: 20,
+        perPage: 5,
+      },
+    };
+  },
+  methods: {
+    next(page) {
+      console.log(page);
+    },
+  },
+};
+</script>
